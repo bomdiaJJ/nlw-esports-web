5b2fb118b0f43bdbce928ccf0c6b2f7cecda17f8
JJ <joaoart42@gmail.com>
2022-09-13 03:13:14 -0300

    :tada: initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 20%, #43E7AD 41%, #E1D55D 70.5%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }},
+  },
+  plugins: [],
+}
